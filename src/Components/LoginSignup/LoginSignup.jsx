a9a58464ea067f9568a4c038a577c35caf6d2c8d
Mohidul Islam <exit.mohidulislam@gmail.com>
2024-01-11 23:30:15 +0600

    update dev to button in login signup page

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -16,23 +16,23 @@ const LoginSignup = () => {
             <div className="underline"></div>
         </div>
         <div className="inputs">
-            {action==="Login"?<div></div>: <div className="input">
+            {action==="Login"?<div></div>: <button className="input">
                 <img src={user_icon} alt="" />
                 <input type="text" placeholder='Name'/>
-            </div>}
-            <div className="input">
-                <img src={email_icon} alt="" />
-                <input type="email" placeholder='Email Id'/>
-            </div>
-            <div className="input">
-                <img src={password_icon} alt="" />
+            </button>}
+            <button className="input">
+                <img src={email_icon}/>
+                <input type='email' placeholder='Email Id'/>
+            </button>
+            <button className="input">
+                <img src={password_icon}/>
                 <input type="password" placeholder='Password'/>
-            </div>
+            </button>
         </div>
         {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
         <div className="submit-container">
-            <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-            <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+            <button className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</button>
+            <button className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</button>
         </div>
     </div>
   )
